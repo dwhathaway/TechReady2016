b2100c89d6bc4db8cfc6f33bf7eb33438381bea9
David Hathaway <David Hathaway@DWHATHAWAY-SP1>
2016-07-25 15:47:42 -0700

    Add project files.

---
+++ TechReady.API/Controllers/SessionsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using TechReady.API.Models;
+
+namespace TechReady.API.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Session> Get()
+        {
+            List<Session> sessions = new List<Session>();
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Sessions"].ConnectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Sessions", conn))
+                {
+                    DataSet ds = new DataSet();
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                    da.Fill(ds);
+
+                    sessions = (from DataRow row in ds.Tables[0].Rows
+
+                                select new Session
+                                {
+                                    Day = row["Day"].ToString(),
+                                    Date = DateTime.Parse(row["Date"].ToString()),
+                                    StartTime = getDate(row["Date"].ToString(), row["Start Time"].ToString()),
+                                    EndTime = getDate(row["Date"].ToString(), row["End Time"].ToString()),
+                                    Code = row["Code"].ToString(),
+                                    Title = row["Title"].ToString(),
+                                    Room = row["Room"].ToString(),
+                                    PrimarySpeaker = row["Primary Speaker s"].ToString(),
+                                    PrimaryTrack = row["Primary Track"].ToString(),
+                                    VirtualTrack = row["Virtual Track"].ToString()
+                                }).ToList();
+                }
+            }
+
+            return sessions;
+        }
+
+        private DateTime getDate(string sessionDate, string startTime)
+        {
+            DateTime myDate;
+
+            var tempDate = DateTime.Parse(sessionDate);
+
+            myDate = DateTime.Parse(string.Format("{0}/{1}/{2}, {3}", tempDate.Month, tempDate.Day, tempDate.Year, startTime));
+
+            return myDate;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TechReady.Common/Session.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TechReady.API.Models
+{
+    public class Session
+    {
+        public string Day { get; set;  }
+
+        public DateTime Date { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set;  }
+
+        public string Room { get; set; }
+
+        public string PrimarySpeaker { get; set; }
+
+        public string PrimaryTrack { get; set; }
+
+        public string VirtualTrack { get; set; }
+    }
+}
